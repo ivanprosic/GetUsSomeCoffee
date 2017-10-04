96fdf2219d477084bfa2c638f0df9fcc5bdb1614
Ivan Prosic <Ivan Prosic>
2017-10-04 13:43:36 +0200

    Minor updates

--- js/details.js
+++ js/details.js
@@ -1,3 +1,6 @@
+var clientId = 'MAZ2HDYFRUBAE25PQQEUIY0XCEE1SZH1Q55MZAFL4CVRZIJL';
+var clientSecret = 'VW44SF5S2MQZ3CEH4MAZ4FJNNVR5LH4IIPBQZJK1GLO335O5';
+
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -9,3 +12,43 @@ function getParameterByName(name, url) {
 }
 
 console.log(getParameterByName('id'));
+
+function getDetails(id, callback){
+  var xhr = new XMLHttpRequest();
+  var url = 'https://api.foursquare.com/v2/venues/' + id;
+
+  var data = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    v: '20170801'
+  };
+
+  xhr.open('GET', url+ '?'+ serializeObjects(data));
+  xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4 && xhr.status == 200) {
+          var details = JSON.parse(xhr.responseText);
+          callback(null, details);
+      }
+  }
+    xhr.send();
+}
+
+getDetails(getParameterByName("id"), function(err, info){
+    if(err){
+      console.log(error);
+      return;
+    }
+    console.log(info);
+});
+
+function serializeObjects(data){
+  var urlParams = "";
+  for (var key in data) {
+      if (urlParams != "") {
+          urlParams += "&";
+      }
+      urlParams += key + "=" + encodeURIComponent(data[key]);
+  }
+  return urlParams;
+
+}
